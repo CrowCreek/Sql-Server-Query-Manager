7c1fd7d63a8325011333a7ac877bc9ede24801d3
Hypnovirus <danshanyfelt@gmail.com>
2015-11-30 20:24:01 -0500

    NuGet work Adjusted for naming of NuGet package

---
+++ CrowCreek.SqlServerQueryManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SQL Server Query Manager")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Crow Creek SQL Server Query Manager")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("141f4000-2b45-4974-846a-921281cc6d86")]
